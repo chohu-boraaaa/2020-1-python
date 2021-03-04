706f01b8becd357eb081e5baa289366d30a17e8b
Jihee Kim <77424107+chohu-boraaaa@users.noreply.github.com>
2021-03-05 01:51:44 +0900

    Add files via upload

---
+++ ch01.py
@@ -0,0 +1,3 @@
+name = input("이름: ")
+age = int(input("나이: "))
+print(name + "님 반가워요. 당신의 나이는 " + str(age) + "살입니다.")
---
+++ ch01_problem1.py
@@ -0,0 +1,5 @@
+radius = int(input("원의 반지름 입력: "))
+area = 3.14 * radius ** 2
+circumference = 2 * 3.14 * radius
+print("원의 넓이: ", area)
+print("원의 둘레: ", circumference)
