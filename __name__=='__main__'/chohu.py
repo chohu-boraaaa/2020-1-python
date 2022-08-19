b3d2653dd3d52286c36c6adc8d9aea82f424fec0
Jihee Kim <77424107+chohu-boraaaa@users.noreply.github.com>
2022-08-19 19:14:54 +0900

    Add files via upload

---
+++ __name__=='__main__'/chohu.py
@@ -0,0 +1,4 @@
+import main_name as mn
+
+a = mn.max_num(55,66)
+print(a)
---
+++ __name__=='__main__'/main_name.py
@@ -0,0 +1,10 @@
+def max_num(x, y):
+    if x>y:
+        return x
+    elif y>x:
+        return y
+    elif x==y:
+        return x
+
+
+print(max_num(30, 88))
